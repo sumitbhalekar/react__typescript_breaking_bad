94c85f66f59935d68c79f704a9ca69196172cc66
Sumit Bhalekar <bhalekar.sumit@5d.solutions>
2022-07-17 14:21:32 +0530

    library removed

--- src/screens/FavouriteCharatcers/FavouriteCharacters.tsx
+++ src/screens/FavouriteCharatcers/FavouriteCharacters.tsx
@@ -1,4 +1,3 @@
-import { Grid, useMediaQuery } from "@mui/material";
 import { useDispatch, useSelector } from "react-redux";
 import HeaderFav from "../../components/HeaderFav.comp";
 import { REMOVE_FAVOURITES } from "../../redux/action_types";
@@ -15,27 +14,18 @@ export default function FavouriteCharacters() {
             payload: favs,
         });
     }
-    const matches = useMediaQuery("(min-width:600px)");
 
     return (
         <>
             <HeaderFav />
             <div style={{ padding: 20, display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>
-                {/* <Grid container spacing={3} > */}
                 {favourites?.map((item: object, index: number) => {
                     return (
-                        // <Grid item xs={6} sm={4}
-                        //     direction="row"
-                        //     maxWidth="lg"
-                        // margin={matches ? "auto auto" : "auto"}
-                        // >
                         <div style={{ padding: 10 }}>
                             <CharacterCard props={item} index={index} onPressFav={() => { removeFavourites(item) }} showFavorites={true} />
                         </div>
-                        // </Grid>
                     )
                 })}
-                {/* </Grid> */}
             </div>
         </>);
 }
